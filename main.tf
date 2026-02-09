e5296cd1dcad1acd07689260e34813dd3c92a090
AVMUser-58970934@LODSPRODMCA.onmicrosoft.com <AVMUser-58970934@LODSPRODMCA.onmicrosoft.com>
2026-02-09 07:01:16 -0800

    Added data import and remove it

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ provider "azurerm" {
   resource_provider_registrations = "core"
 }
 
+locals {
+  subnets = cidrsubnets("10.0.0.0/24", 8, 7)
+}
+
 resource "azurerm_resource_group" "demo" {
   for_each = var.resource_groups
   name     = "${var.prefix}_${each.value}"
