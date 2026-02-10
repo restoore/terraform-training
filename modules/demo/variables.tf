1f4a1f4904324110811e7f1490d3f195ba07bb06
AVMUser-58970934@LODSPRODMCA.onmicrosoft.com <AVMUser-58970934@LODSPRODMCA.onmicrosoft.com>
2026-02-10 01:28:23 -0800

    Added local module

---
+++ modules/demo/variables.tf
@@ -0,0 +1,34 @@
+variable resource_groups {
+  type        = map(string)
+  description = "The resource groups to deploy"
+}
+
+variable "prefix" {}
+
+variable "region" {
+  type        = string
+  default     = "UK South"
+  description = "The Azure region to deploy resources"
+  validation {
+    condition     = contains(["UK South", "UK West", "North Europe", "West Europe", "East US", "West US"], var.region)
+    error_message = "Invalid region"
+  }
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "A map of tags"
+}
+
+variable "virtual_networks" {
+  type = map(object({
+    name               = string
+    resource_group_key = string
+    address_space      = list(string)
+    subnets = map(object({
+      name           = optional(string)
+      address_prefix = string
+    }))
+  }))
+  description = "The virtual networks to deploy"
+}
